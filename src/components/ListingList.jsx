b08f7725269dcfa65672f19396a3491dc4fea71a
carlos.tangarife <carlos@campminder0.onmicrosoft.com>
2024-11-22 19:09:41 -0500

    Refactor ListingList component to simplify rendering logic and handle empty listings

--- src/components/ListingList.jsx
+++ src/components/ListingList.jsx
@@ -1,15 +1,15 @@
 import { ListingCard } from '@/components/ListingCard';
 
 export const ListingList = ({ listings = [] }) => {
+  if (listings.length === 0) {
+    return <p>No listings found.</p>;
+  }
+
   return (
     <div className='flex flex-wrap justify-center gap-4'>
-      {listings.length > 0 ? (
-        listings.map((listing) => (
-          <ListingCard key={listing.id} listing={listing} />
-        ))
-      ) : (
-        <p>No listings found.</p>
-      )}
+      {listings.map((listing) => (
+        <ListingCard key={listing.id} listing={listing} />
+      ))}
     </div>
   );
 };
